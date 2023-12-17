239f1f1ad2c0f352ae2deca2fdc7a935ebe586a7
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-12-17 20:50:16 -0300

    feat: vite-plugin-vue-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import Vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import Icons from 'unplugin-icons/vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,7 @@ export default defineConfig({
     }),
     Components(),
     Icons({ compiler: 'vue3' }),
+    VueDevTools(),
   ],
   resolve: {
     alias: {
