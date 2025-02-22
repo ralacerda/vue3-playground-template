d70f330859874055ebc0ba8213e80c4b9f79e509
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-02-22 10:57:43 -0300

    chore: removing oxlint from eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,5 @@
 import pluginVue from 'eslint-plugin-vue';
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
-import oxlint from 'eslint-plugin-oxlint';
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default defineConfigWithVueTs(
@@ -16,6 +15,5 @@ export default defineConfigWithVueTs(
 
   pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
-  oxlint.configs['flat/recommended'],
   skipFormatting
 );
