bf8f0dbb572e903817ba87efe10d4257c54f6018
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-07-16 19:24:53 -0300

    chore: picocss and new card component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,30 @@
 <script setup lang="ts">
 import { useCounterStore } from "@/stores/counter";
 import PinapleIcon from "~icons/mdi/fruit-pineapple";
+import SimpleCard from "./SimpleCard.vue";
 
 const counterStore = useCounterStore();
 </script>
 
 <template>
-  <main>
+  <main class="container">
     <h1>Vue 3 Playground Template</h1>
-    <div class="card-wrapper">
-      <div class="card">
-        <h2><PinapleIcon style="color: yellow" /> Pinia Store</h2>
-        <p>Count is {{ counterStore.count }}</p>
-        <button @click="counterStore.increment">Hello</button>
-      </div>
-    </div>
+    <SimpleCard class="pinia-card">
+      <h2><PinapleIcon style="color: yellow" /> Pinia Store</h2>
+      <p>Count is {{ counterStore.count }}</p>
+      <button @click="counterStore.increment">Increase</button>
+    </SimpleCard>
   </main>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 main {
-  display: grid;
-  place-items: center;
-  gap: 2rem;
+  margin-top: 2rem;
+  text-align: center;
+}
+
+.pinia-card {
   margin-top: 4rem;
+  margin-inline: auto;
 }
 </style>
---
+++ src/components/SimpleCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 2rem;
+  border-radius: 0.25rem;
+  background-color: var(--pico-card-background-color);
+  border: 1px solid var(--pico-muted-border-color);
+  box-shadow: var(--pico-box-shadow);
+  width: 300px;
+  text-align: center;
+}
+</style>
